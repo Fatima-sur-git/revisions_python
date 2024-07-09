862b4006f94a0dfe91e2354fc7fb68ca5713c183
Fatima-sur-git <0753habiba.fatima@gmail.com>
2024-07-09 03:02:36 +0000

    presque terminé les methodes de la classe customer

--- project/customer.py
+++ project/customer.py
@@ -2,13 +2,16 @@ class Customer:
 
     customers = []
 
-    def __init__(self,code,nom,telephone,adresse,email):
+    customers_num = 0
+
+    def __init__(self,code,nom,telephone,adresse,email,solde=0):
 
         self.id = code
         self.name = nom
         self.phone = telephone
         self.adress = adresse
         self.mail = email
+        self.balance = solde
 
     def display_customers(customers):
 
@@ -24,18 +27,15 @@ class Customer:
                     Téléphone : {customer["phone"]}
                     Adresse : {customer["adress"]}
                     Email : {customer["mail"]}
+                    Solde : {customer["balance"]}
                     """)
         else:
             print("Il n'y a aucun client!")
 
-    @classmethod
-    def add_customer(cls) :
+    
+    def add_customer(self) :
 
-        your_id = input("Veuillez svp entrer le code client : ")
-
-        while not your_id.isalnum():
-            print("Mauvais code !")
-            your_id = input("Veuillez svp entrer le code client : ")
+        your_id = f"C0{Customer.customers_num}"
 
         your_name = input("Veuillez svp entrer le nom du client : ")
 
@@ -51,23 +51,22 @@ class Customer:
 
         your_adress = input("Veuillez svp entrer l'adresse du client : ")
 
-        while not your_adress.isalpha():
-            print("Mauvaise entrée !")
-            your_adress = input("Veuillez svp entrer l'adresse du client : ")
-
         your_mail = input("Veuillez svp entrer l'email du client : ")
 
         new_customer = {
-                    "Code" : your_id,
-                    "Nom" : your_name,
-                    "Téléphone" : your_phone,
-                    "Adresse" : your_adress,
-                    "Email" : your_mail
+                    "id" : your_id,
+                    "name" : your_name,
+                    "phone" : your_phone,
+                    "adress" : your_adress,
+                    "mail" : your_mail,
+                    "balance" : self.balance
         }
 
-        cls.customers.append(new_customer)
+        Customer.customers.append(new_customer)
 
-        print("Client ajouté avec succès !")
+        Customer.customers_num += 1
+
+        print(f"Client {your_id} ajouté avec succès !")
 
     @classmethod
     def delete_customer(cls) :
@@ -82,13 +81,98 @@ class Customer:
 
             if customer["id"] == to_delete :
                 cls.customers.remove(customer)
-                print("***Client supprimé !***")
+                print(f"***Client {customer["id"]} supprimé !***")
+                break
 
-            else :
-                print("***Client inexistant !***")
+        else :
+            print(f"***Client {to_delete} inexistant !***")
 
+    @classmethod
+    def change_info(cls):
+
+        to_change = input("Veuillez svp entrer le code du client à modifier : ")
+
+        for customer in cls.customers :
+
+            if customer["id"] == to_change : 
+
+                print("""
+                    Quelle information voulez vous changer ?
+                        1- le nom
+                        2- le numero de téléphone
+                        3- l'adresse
+                        4- l'email
+                    """)
+                numero = input("Veuillez svp entrer le numero : ")
+
+                while not (numero.isdigit() and 1 <= int(numero) <= 4) :
+                    print("Veuillez choisir un chiffre correspondant aux propositions !")
+                    numero = input("Veuillez svp entrer le numero : ")
+
+                if int(numero) == 1 :
+
+                    new_name = input("Veuillez svp entrer le nouveau nom du client : ")
+
+                    while not new_name.isalpha():
+                        print("Mauvaise entrée !")
+                        new_name = input("Veuillez svp entrer le nouveau nom du client : ")
+
+                    customer["name"] = new_name
+
+                    print(f"***Nom du client {customer["id"]} modifié !***")
+
+                elif int(numero) == 2 :
+
+                    new_phone = input("Veuillez svp entrer le nouveau numero du client : ")
+
+                    while not (new_phone.isdigit() and len((new_phone)) == 9):
+                        print("Mauvaise entrée !")
+                        new_phone = input("Veuillez svp entrer le nouveau numero du client : ")
+
+                    customer["phone"] = new_phone
+
+                    print(f"***Numero du client {customer["id"]} modifié !***")
+
+                elif int(numero) == 3 :
+
+                    new_adress = input("Veuillez svp entrer la nouvelle adresse du client : ")
+
+                    customer["adress"] = new_adress
+
+                    print(f"***Adresse du client {customer["id"]} modifiée !***")
+
+                else :
+                    new_mail = input("Veuillez svp entrer le nouvel email du client : ")
+
+                    customer["mail"] = new_mail
+
+                    print(f"***Email du client {customer["id"]} modifié !***")
+        else : 
+            print("***Client inexistant !***")
+
+    def customer_balance(customers):
+        the_customer = input("Veuillez svp entrer le code du client dont vous voulez voir le solde : ")
+
+        for customer in customers:
+             if customer["id"]==the_customer:
+                print(f"""
+                    Code : {customer["id"]}
+                    Nom : {customer["name"]}
+                    Solde : {customer["balance"]}
+                """)
+        
+        else:
+            print("***Client inexistant !***")
+
+
+
+
+Customer.add_customer(self="habib")
 Customer.add_customer()
-Customer.display_customers()
+Customer.display_customers(Customer.customers)
+Customer.change_info()
+Customer.display_customers(Customer.customers)
+Customer.customer_balance(Customer.customers)
 #Customer.delete_customer()
 #Customer("D1","TARIK","781286853","DAKAR","0753habiba.fatima@gmail.com")
 #print(Customer.customers)
